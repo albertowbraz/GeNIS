6fa5d9a95e58b31e3252f22d25ecf94fd93a4481
Herbeth Milhome <h2milhome@gmail.com>
2014-07-06 16:03:42 -0300

    add consulta

--- src/main/java/br/genis/servicos/UsuarioDao.java
+++ src/main/java/br/genis/servicos/UsuarioDao.java
@@ -1,12 +1,30 @@
 package br.genis.servicos;
 
+import javax.ejb.Stateless;
+
 import br.genis.modelos.Usuario;
 
-public class UsuarioDao extends ServicoPadrao<Usuario>{
+@Stateless
+public class UsuarioDao extends GenericDAO<Usuario>{
+	
+	private static final long serialVersionUID = 657115834272358690L;
 
-	@Override
-	Boolean validar(Usuario entidade) throws ServicoBaseException {
-		return true;
+	public Usuario verificaLogin(String login, String senha) {
+		Usuario usuario = null;
+
+		try {
+			
+			usuario = em
+					.createQuery("select u from Usuario u where u.login = :login and u.senha = :senha", Usuario.class)
+						.setParameter("login", login)
+						.setParameter("senha", senha)
+						.getSingleResult();
+			
+		} catch (Exception e) {
+			// TODO 
+			System.out.println(" ################### FALHA AO CARREGAR O USUARIO \n ERROR: " + e);
+		}
+		return usuario;
 	}
 
 }
